6a55e92d4d96e3d2ddf6ad2f8d6ea09b3a37d885
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-08-20 17:39:03 +0530

    UI Improvements 🥽

--- src/main/java/com/abhyudayasharma/tictactoe/Game.java
+++ src/main/java/com/abhyudayasharma/tictactoe/Game.java
@@ -8,23 +8,30 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Insets;
+import java.util.Vector;
 
 @ParametersAreNonnullByDefault
 class Game {
     private PlayerType startPlayer = null;
     private PlayerType currentPlayer = null;
     private final JFrame gameFrame = new JFrame("Tic Tac Toe");
-    private final List<Integer> humanMoves = new ArrayList<>(9);
-    private final List<Integer> computerMoves = new ArrayList<>(9);
+    private final JList<Integer> humanMovesList = new JList<>();
+    private final JList<Integer> computerMovesList = new JList<>();
+    private final Vector<Integer> humanMoves = new Vector<>(9);
+    private final Vector<Integer> computerMoves = new Vector<>(9);
 
     @Nonnegative
     private final int[][] magicSquare = MagicSquare.getMagicSquare(3);
@@ -41,15 +48,43 @@ class Game {
             JButton button = new JButton();
             button.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 44));
             button.setBackground(Color.WHITE);
+            button.setPreferredSize(new Dimension(125, 125));
+            button.setMinimumSize(button.getPreferredSize());
             addOnClickListener(button, i / 3, i % 3);
             boxes.add(button);
             boxes.setBackground(Color.WHITE);
         }
 
-        gameFrame.setPreferredSize(new Dimension(400, 400));
+        JPanel lists = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        lists.add(new JLabel("Human moves"), gbc);
+        gbc.gridx++;
+        lists.add(new JLabel("Computer moves"));
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridheight = 7;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        var humanMovesScrollPane = new JScrollPane(humanMovesList);
+        humanMovesScrollPane.setPreferredSize(new Dimension(100, 100));
+        lists.add(humanMovesScrollPane, gbc);
+        gbc.gridx++;
+
+        var computerMovesScrollPane = new JScrollPane(computerMovesList);
+        computerMovesScrollPane.setPreferredSize(new Dimension(100, 100));
+        lists.add(computerMovesScrollPane, gbc);
+        lists.setBackground(Color.WHITE);
+
         gameFrame.setLayout(new BoxLayout(gameFrame.getContentPane(), BoxLayout.LINE_AXIS));
         gameFrame.setResizable(false);
         gameFrame.add(boxes);
+        gameFrame.add(lists);
         gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gameFrame.setVisible(true);
         gameFrame.pack();
@@ -79,23 +114,24 @@ class Game {
             }
 
             currentPlayer = getNextPlayer();
-
-            // FIXME we don't need console logs, move it to the GUI
-            System.out.println("Human moves: " + humanMoves);
-            System.out.println("Computer moves: " + computerMoves);
+            humanMovesList.setListData(humanMoves);
+            computerMovesList.setListData(computerMoves);
 
             // FIXME check if the game has been won
             if (humanMoves.size() + computerMoves.size() == 9) {
                 gameOver();
             }
         });
+
+        humanMovesList.setEnabled(false);
+        computerMovesList.setEnabled(false);
     }
 
     private void gameOver() {
         // FIXME
         JOptionPane.showMessageDialog(gameFrame, "Game Over! ${WINNER} won.", "Game Over",
             JOptionPane.PLAIN_MESSAGE);
-        gameFrame.setVisible(false);
+        gameFrame.dispose();
     }
 
     @CheckForNull
