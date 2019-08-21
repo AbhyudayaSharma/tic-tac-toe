ca87923caeb2872a6819460c31ac6a098cc6ca0d
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-08-21 22:37:37 +0530

    Add JavaDocs to MagicSquare.java

--- src/main/java/com/abhyudayasharma/tictactoe/MagicSquare.java
+++ src/main/java/com/abhyudayasharma/tictactoe/MagicSquare.java
@@ -10,6 +10,11 @@ class MagicSquare {
     private int[][] matrix;
     private static int expectedSum;
 
+    /**
+     * Private constructor for the Magic square.
+     *
+     * @param n the size of the magic square to be generated.
+     */
     private MagicSquare(int n) {
         int max = n * n;
         expectedSum = (int) (n * (Math.pow(n, 2) + 1)) / 2;
@@ -18,14 +23,36 @@ class MagicSquare {
         placeNextOdd(0, (n - 1) / 2, matrix, n, max);
     }
 
+    /**
+     * Returns the expected sum of every diagonal, row and column of the Magic Square.
+     *
+     * @return the expected sum of every diagonal, row and column of the Magic Square.
+     */
     static int getExpectedSum() {
         return expectedSum;
     }
 
+    /**
+     * Calculates and returns a new magic square object. The objects produced by this methods
+     * are isomorphic to one another but are not eht same objects.
+     *
+     * @param n the size of the magic square to be generated. Even values do not produce expected results.
+     * @return a new magic square
+     */
+    @SuppressWarnings("SameParameterValue") // tic tac toe uses a 3x3 magic square only
     static int[][] getMagicSquare(int n) {
         return new MagicSquare(n).matrix;
     }
 
+    /**
+     * Recursive method for placing digits in the magic square.
+     *
+     * @param r      the row index
+     * @param c      the column index
+     * @param matrix matrix that will eventually become a magic square
+     * @param n      the size of the magic square to be constructed
+     * @param max    the maximum possible value in the magic square.
+     */
     private void placeNextOdd(int r, int c, int[][] matrix, int n, int max) {
         if (mainCnt == max) {
             return;
